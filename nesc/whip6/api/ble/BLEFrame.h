84cbd0a1a8749156306b4fde50caa93f5b5e24a4
Szymon Acedański <accek@mimuw.edu.pl>
2017-03-22 17:15:24 +0100

    Extracted ble_adv_pdu_t

--- nesc/whip6/api/ble/BLEFrame.h
+++ nesc/whip6/api/ble/BLEFrame.h
@@ -4,7 +4,7 @@
  * Copyright (c) 2012-2016 InviNets Sp z o.o.
  * All rights reserved.
  *
- * This file is distributed under the terms in the attached LICENSE     
+ * This file is distributed under the terms in the attached LICENSE
  * files. If you do not find these files, copies can be found by writing
  * to technology@invinets.com.
  */
@@ -16,19 +16,23 @@
 
 #define BLE_MAX_FRAME_LEN 43
 
-typedef union _ble_pdu_s {
+typedef struct _ble_adv_pdu_s {
     struct {
-        struct {
-            unsigned type : 4;
-            unsigned reserved1 : 2;
-            unsigned TxAdd : 1;
-            unsigned RxAdd : 1;
-            unsigned length : 6;
-            unsigned reserved0 : 2;
-        } __attribute__((packed)) header;
-        _ble_address_t address;
-        uint8_t data[31];
-    } __attribute__((packed)) adv;
+        unsigned type : 4;
+        unsigned reserved1 : 2;
+        unsigned TxAdd : 1;
+        unsigned RxAdd : 1;
+        unsigned length : 6;
+        unsigned reserved0 : 2;
+    } __attribute__((packed)) header;
+    _ble_address_t address;
+    uint8_t data[31];
+} __attribute__((packed)) _ble_adv_pdu_t;
+typedef _ble_adv_pdu_t _ble_adv_pdu_t_xdata;
+typedef _ble_adv_pdu_t_xdata ble_adv_pdu_t;
+
+typedef union _ble_pdu_s {
+    _ble_adv_pdu_t adv;
     // TODO: data channel pdu.
 } __attribute__((packed)) _ble_pdu_t;
 typedef _ble_pdu_t _ble_pdu_t_xdata;
